d9582a6bd93728140488827e2da01c72af96cd81
V.Akekachai <vijityannapon@gmail.com>
2016-09-01 14:38:29 +0700

    Mockup Layout Google Map

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class HomeController extends Controller
+{
+    
+    public function index() {
+
+    	$data = [];
+
+    	return view('home', $data);
+
+    }
+}
