70205ca7cf9806ffc35eca341186fa03cc29be20
V.Akekachai <vijityannapon@gmail.com>
2016-09-05 08:16:01 +0000

    Update CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,24 +6,18 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <style>
-    .map{
-        width: 100%;
-        height: 400px;
-        position: relative; overflow: hidden;
-     }
-    </style>
+        <link rel="stylesheet" href="{{ URL::asset('/assets/css/custom.css') }}" />
+
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfA9UUkERXuMPUWAvgBIhNBBtIF7ymrQw&libraries=places"></script>
 
         <script type="text/javascript" src="{{ URL::asset('/assets/js/script.js') }}"></script>
+
     </head>
 
     <body>
-
         <dlv class="row">
-
             <div class="col-sm-12">
                 <div class="map" id="map"></div>
             </div>
